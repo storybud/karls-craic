82eceadfbaa5b103629a3f067d9c8699da70f9fe
karlalmanac <karl@almanac.digital>
2023-02-10 12:43:53 +0000

    fix: api update

--- src/_data/blog.js
+++ src/_data/blog.js
@@ -2,8 +2,8 @@ const { default: axios } = require('axios');
 
 module.exports = async () => {
   try {
-    const res = await axios.get('http://127.0.0.1:1337/api/blogs');
-    // const res = await axios.get('https://strapi-blog-pr9ax.ondigitalocean.app/api/blogs');
+    // const res = await axios.get('http://127.0.0.1:1337/api/blogs');
+    const res = await axios.get('https://strapi-blog-pr9ax.ondigitalocean.app/api/blogs');
     return res.data;
   } catch (error) {
     console.error(error);
